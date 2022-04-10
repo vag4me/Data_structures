b8a620afd478468a7e05b886beb86d9f259c117a
VK <93388918+vag4me@users.noreply.github.com>
2022-04-10 22:16:57 +0300

    Add files via upload

---
+++ bubble_sort.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void bubbleSort(int array[], int size) {
+
+  for (int step = 0; step < size - 1; ++step) {
+    for (int i = 0; i < size - step - 1; ++i) {
+      if (array[i] > array[i + 1]) {
+        int temp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
+      }
+    }
+  }
+}
+
+
+int main() {
+	return 0;
+}
